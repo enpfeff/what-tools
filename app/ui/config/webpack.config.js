e209da553af51c377fa20aabe851b294d75ba268
Ian Pfeffer <ian.pfeffer@netscout.com>
2016-11-01 16:32:01 -0600

    started gulp processes for build, dev, production

--- app/ui/config/webpack.config.js
+++ app/ui/config/webpack.config.js
@@ -4,30 +4,45 @@
  * @author Ian Pfeffer
  */
 "use strict";
+const _ = require('lodash');
 const webpack = require('webpack');
 
-module.exports = {
-    // creates source maps
-    devtool: "eval",
+function webpackConfig(prod, watch = false) {
+    let config = {
+        // creates source maps
+        devtool: "eval",
 
-    // build output
-    stats: {
-        colors: true,
-    },
-    output: {
-        filename: 'bundle.js',
-        publicPath: 'http://localhost:3001/'
-    },
-    watch: true,
-    loaders: [{
-        test: /\.js$/,
-        loader: ['ng-annotate', 'nginject?deprecate', "babel-loader?presets[]=es2015,cacheDirectory"]
-    }],
-    plugins: [
-        // Sets global variables
-        new webpack.ProvidePlugin({
-            _: 'lodash',
-            angular: 'angular'
-        }),
-    ]
-};
+        // build output
+        stats: {
+            colors: true,
+        },
+        output: {
+            filename: 'bundle.js',
+            publicPath: 'http://localhost:3001/'
+        },
+        watch: watch,
+        loaders: [{
+            test: /\.js$/,
+            loader: ['ng-annotate', 'nginject?deprecate', "babel-loader?presets[]=es2015,cacheDirectory"]
+        }],
+        plugins: [
+            // Sets global variables
+            new webpack.ProvidePlugin({
+                _: 'lodash',
+                angular: 'angular'
+            }),
+        ]
+    };
+
+    if(prod) return productionConfig(config);
+
+    return config;
+}
+
+function productionConfig(config) {
+    const OMIT = ['devtool', 'watch', 'output.publicPath'];
+
+    return _.omit(config, OMIT);
+}
+
+module.exports = webpackConfig;
--- app/ui/gulpfile.js
+++ app/ui/gulpfile.js
@@ -6,17 +6,35 @@
 "use strict";
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
+const gutil = require('gulp-util');
+const PRODUCTION = 'production';
 
 const tasks = {
-    build: () => {
+    build: (watch) => {
+        const config = getConfig(isProd(), watch);
 
+        return gulp.src('./app.js')
+            .pipe(webpack(config))
+            .pipe(gulp.dest('./dist/'))
     }
 };
 
-gulp.task('default', () => {
-    return gulp.src('./app.js')
-        .pipe(webpack(require('./config/webpack.config')))
-        .pipe(gulp.dest('./dist/'))
-});
+function getConfig(isProd, watch = false) {
+    const configFactory = require('./config/webpack.config');
+    return configFactory(isProd, watch);
+}
+
+function isProd() {
+    const UI_ENV = process.env.UI_ENV;
+    const isProd = UI_ENV === PRODUCTION;
+
+    if(isProd) gutil.log(gutil.colors.red('---- Production Enabled ----'));
+    return isProd;
+}
+
+gulp.task('build', () => tasks.build(false));
+gulp.task('dev', () => tasks.build(true));
+
+gulp.task('default',() => tasks.build(false));
 
 
