5ff7651c6ee47722012a4e06274dda4f13ef535d
Ian Pfeffer <ian.pfeffer@netscout.com>
2017-01-18 12:15:51 -0700

    +x

--- tools/install.sh
+++ tools/install.sh
@@ -7,15 +7,19 @@ fi
 
 install_dir=/opt/what-tools/tools
 bin=${install_dir}/bin
+cwd=`pwd`
+
+cd ../tools
 
 mkdir -p ${install_dir}
 cp -rf config lib entries bin package.json ${install_dir}
 chmod -R 775 ${install_dir}
 chown -R media:data ${install_dir}
 cd ${install_dir}
-npm install
+sudo -u media source $HOME/.bashrc && npm install
 chmod -R 775 ${install_dir}/node_modules
 
 cp -f ${bin}/plex-updater.sh /usr/bin/plex-updater && chmod 775 /usr/bin/plex-updater
 cp -f ${bin}/process-torrent.sh /usr/bin/process-torrent && chmod 775 /usr/bin/process-torrent
 cp -f ${bin}/uninstall-what-tools.sh /usr/bin/uninstall-what-tools && chmod 775 /usr/bin/uninstall-what-tools
+cd $cwd
