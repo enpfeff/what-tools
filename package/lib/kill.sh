22686268735b3487b43b0b8996ae805ce946815c
Ian Pfeffer <ian.pfeffer@netscout.com>
2017-01-18 10:27:45 -0700

    adsf

--- package/lib/kill.sh
+++ package/lib/kill.sh
@@ -3,8 +3,9 @@
 function killWebApp() {
     echo -e "WebApp"
     source ${HOME}/.bashrc
-    nvm use 6.9.4
+
     if [ -d /opt/what-tools/app/server ]; then
+        echo -e "Found Webapp folder need to stop it..."
         cd /opt/what-tools/app/server
         npm run kill
     fi
